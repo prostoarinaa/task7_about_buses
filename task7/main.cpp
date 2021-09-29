a6446e54128277ca600cbf63445fe8c847c4ae26
Motorina Arina <arinamotorinaaaaa@gmail.com>
2021-09-30 00:52:55 +0300

    можно еще разбить на классы конечно..

--- task7/main.cpp
+++ task7/main.cpp
@@ -16,15 +16,58 @@ struct Bus {
     int countOfTonInABus;
     int numberOfTypeProduct;
 };
+
 struct Product {
     int howMuch;
     int whatProduct;
 };
+
 struct Gate {
     int whatProduct = 0; //1,2,3
     int howMuch = 0;
     bool LoadingOrUploading = 0;
 };
+
+vector <Gate> Gat(vector <Gate> Gates);
+vector <Product> Prod(vector <Product> Products);
+
+vector <Gate> Gates ;
+vector <Product> Products ;
+queue <Bus> Buses1 = {};
+int TIME = 0;
+
+class BUSES {
+public:
+    queue <Bus> GenerateBuses (queue <Bus>& Buses);
+    queue <Bus> GenerateBuses2 (queue <Bus>& Buses);
+    queue <Bus> AddNewBus(queue <Bus> Buses);
+    Bus ReturnFirstBus(queue <Bus> Buses);
+    void WhatEnter (Bus bus);
+    void WhatExit();
+};
+
+void thread1();
+void thread2();
+void thread3();
+void START();
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    START();
+    return 0;
+}
+//  thread th(print, a, b);
+//  th.detach();//по очереди каждая итерация оборвется th если разное количество итераций
+//  th.join();//если после вызывать то как будто в одном потоке будет надо ставить где нужн
+//  cout<<this_thread::get_id()<<"  "<<i<<endl;   //вывод текущего потока
+//  this_thread::sleep_for(chrono::milliseconds(1000));
+//  th.join();
+//  this_thread::sleep_for(chrono::milliseconds(1000));  //приостанавливает работу текущего потока на нек время
+
+
+/*------------------------------------------------------------------------------------------------*/
+
+
 vector <Gate> Gat(vector <Gate> Gates) {
     for (int i =0;i < 5;i++) {
         Gate gate;
@@ -44,6 +87,7 @@ vector <Gate> Gat(vector <Gate> Gates) {
     }
     return Gates;
 };
+
 vector <Product> Prod(vector <Product> Products) {
     for (int i =0;i < 3;i++) {
         Product product;
@@ -63,148 +107,9 @@ vector <Product> Prod(vector <Product> Products) {
     return Products;
 };
 
-vector <Gate> Gates ;
-vector <Product> Products ;
-queue <Bus> Buses1 = {};
-int TIME = 0;
 
+/*------------------------------------------------------------------------------------------------*/
 
-class BUSES {
-public:
-
-    queue <Bus> GenerateBuses (queue <Bus>& Buses) {
-        this_thread::sleep_for(chrono::milliseconds(10));
-        int countOfBus = rand()%30 + 1;
-        cout << "----Количество автобусов, генерируемых каждые 24 часа = " << countOfBus <<"-----"<< endl;
-        for (int i = 0; i < countOfBus; i++) {
-            Buses = AddNewBus(Buses);
-        }
-    return Buses;
-    };
-    
-    queue <Bus> GenerateBuses2 (queue <Bus>& Buses) {
-            unsigned long k = Buses.size();
-            unsigned long g = Gates.size();
-            for (int i = 0; i < k; i++) {
-                Bus B = Buses.front();
-                WhatEnter(B);
-                cout << "---------Текущее положение ворот, машины и склада с продуктами--------" << endl;
-                this_thread::sleep_for(chrono::milliseconds(10));
-                cout << "BUS № " << i << ", Тонн " << B.countOfTonInABus << ", Продукт № " << B.numberOfTypeProduct << endl << endl;
-                for (int i = 0; i < 3; i++) {
-                    Product product = Products[i];
-                    cout <<"---PRODUCT на складе, тип №: " << product.whatProduct << " , в количестве " << product.howMuch << " ----" << endl;
-                }
-                for (int i = 0; i < g; i++) {
-                    Gate G = Gates[i];
-                    this_thread::sleep_for(chrono::milliseconds(10));
-                    cout << "GATE № " << i << ", Продукт № " << G.whatProduct  << ", положение " << G.LoadingOrUploading << endl;
-                    this_thread::sleep_for(chrono::milliseconds(10));
-                }
-                cout << "-------------------------------------------------------------------" << endl << endl<< endl;
-                Buses.pop();
-            }
-        return Buses;
-    };
-    
-    queue <Bus> AddNewBus(queue <Bus> Buses) {
-        Bus bus;
-        bus.countOfTonInABus = 10;
-        bus.numberOfTypeProduct = rand()%3 + 1;
-        Buses.push(bus);
-        return Buses;
-    };
-    
-    Bus ReturnFirstBus(queue <Bus> Buses) {
-        Bus firstBus = Buses.front();
-        return firstBus;
-    };
-    
-    void WhatEnter (Bus bus) {
-        this_thread::sleep_for(chrono::milliseconds(1000));
-        if (bus.numberOfTypeProduct == 1) {
-            
-            if (Products[0].howMuch < 50) {
-                Gates[1].whatProduct = 1;
-                Gates[1].LoadingOrUploading = 1;
-                Products[0].howMuch += bus.countOfTonInABus;
-                bus.countOfTonInABus = 0;
-            }
-            else if ((Products[0].howMuch < 50 && Gates[0].whatProduct == 1) || Gates[0].howMuch == 0) {
-                Gates[0].whatProduct = 1;
-                Gates[0].LoadingOrUploading = 1;
-                Products[0].howMuch += bus.countOfTonInABus;
-                bus.countOfTonInABus = 0;
-            }
-            else if ((Products[0].howMuch < 50 && Gates[4].whatProduct == 1) || Gates[4].howMuch == 0) {
-                Gates[4].whatProduct = 1;
-                Gates[4].LoadingOrUploading = 1;
-                Products[0].howMuch += bus.countOfTonInABus;
-                bus.countOfTonInABus = 0;
-            }
-        }
-        else if (bus.numberOfTypeProduct == 2) {
-            if (Products[1].howMuch < 50) {
-                Gates[2].whatProduct = 2;
-                Gates[2].LoadingOrUploading = 1;
-                Products[1].howMuch += bus.countOfTonInABus;
-                bus.countOfTonInABus = 0;
-            }
-            else if ((Products[1].howMuch < 50 && Gates[0].whatProduct == 2) || Gates[0].howMuch == 0) {
-                Gates[0].whatProduct = 2;
-                Gates[0].LoadingOrUploading = 1;
-                Products[1].howMuch += bus.countOfTonInABus;
-                bus.countOfTonInABus = 0;
-            }
-            else if ((Products[1].howMuch < 50 && Gates[4].whatProduct == 2) || Gates[4].howMuch == 0) {
-                Gates[4].whatProduct = 2;
-                Gates[4].LoadingOrUploading = 1;
-                Products[1].howMuch += bus.countOfTonInABus;
-                bus.countOfTonInABus = 0;
-            }
-        }
-        else if (bus.numberOfTypeProduct == 3) {
-            if (Products[2].howMuch < 50) {
-                Gates[3].whatProduct = 3;
-                Gates[3].LoadingOrUploading = 1;
-                Products[2].howMuch += bus.countOfTonInABus;
-                bus.countOfTonInABus = 0;
-            }
-            else if ((Products[2].howMuch < 50 && Gates[0].whatProduct == 3) || Gates[0].howMuch == 0) {
-                Gates[0].whatProduct = 3;
-                Gates[0].LoadingOrUploading = 1;
-                Products[2].howMuch += bus.countOfTonInABus;
-                bus.countOfTonInABus = 0;
-            }
-            else if ((Products[2].howMuch < 50 && Gates[4].whatProduct == 3) || Gates[4].howMuch == 0) {
-                Gates[4].whatProduct = 3;
-                Gates[4].LoadingOrUploading = 1;
-                Products[2].howMuch += bus.countOfTonInABus;
-                bus.countOfTonInABus = 0;
-            }
-        }
-        this_thread::sleep_for(chrono::milliseconds(1000));
-        cout << endl << "/////////------Машина выгружена за 2 часа------/////////" << endl << endl;
-        
-    };
-    void WhatExit() {
-        this_thread::sleep_for(chrono::milliseconds(2000));
-        int maxOfGates = 0;
-        int max = 0;
-            for (int i=0; i<5; i++){
-                if (Products[i].howMuch >= max) {
-                    max = Products[i].howMuch;
-                    maxOfGates = i;
-                }
-            }
-        if (Products[maxOfGates].howMuch >= 50) {
-            Products[maxOfGates].howMuch -= 10;
-           // Products[maxOfGates].LoadingOrUploading = 0;
-        }
-    };
-    
-    
-};
 
 void thread1() {
     int count = 0;
@@ -243,22 +148,8 @@ void thread2() {
             }
             cout<< "////////////////Конец работы 2 потока///////////////"<< endl;
         }
-        
     }
-}
-
-void thread21() {
-    int count = 0;
-    this_thread::sleep_for(chrono::milliseconds(2000));
-    cout<< "////////////////Начало работы 2.1 потока///////////////"<< endl;
-    BUSES B;
-    while(true) {
-        count++;
-        B.GenerateBuses2(Buses1);
-        this_thread::sleep_for(chrono::milliseconds(2000));
-        cout<< "////////////////Конец работы 2.1 потока///////////////"<< endl;
-    }
-}
+};
 
 void thread3() {
     BUSES B;
@@ -278,8 +169,9 @@ void thread3() {
             cout<< "////////////////Конец работы 3 потока///////////////"<< endl;
         }
     }
-}
-void START(){
+};
+
+void START() {
     thread t1(thread1);
     thread t2(thread2);
     thread t3(thread3);
@@ -288,16 +180,137 @@ void START(){
     t1.join();
 }
 
-int main() {
-    setlocale(LC_ALL, "Russian");
-    START();
-    return 0;
-}
-//  thread th(print, a, b);
-//  th.detach();//по очереди каждая итерация оборвется th если разное количество итераций
-//  th.join();//если после вызывать то как будто в одном потоке будет надо ставить где нужн
-//  cout<<this_thread::get_id()<<"  "<<i<<endl;   //вывод текущего потока
-//  this_thread::sleep_for(chrono::milliseconds(1000));
-//  th.join();
-//  this_thread::sleep_for(chrono::milliseconds(1000));  //приостанавливает работу текущего потока на нек время
+
+/*------------------------------------------------------------------------------------------------*/
+
+
+queue <Bus> BUSES::GenerateBuses (queue <Bus>& Buses) {
+    this_thread::sleep_for(chrono::milliseconds(10));
+    int countOfBus = rand()%30 + 1;
+    cout << "----Количество автобусов, генерируемых каждые 24 часа = " << countOfBus <<"-----"<< endl;
+    for (int i = 0; i < countOfBus; i++) {
+        Buses = AddNewBus(Buses);
+    }
+return Buses;
+};
+
+queue <Bus> BUSES::GenerateBuses2 (queue <Bus>& Buses) {
+        unsigned long k = Buses.size();
+        unsigned long g = Gates.size();
+        for (int i = 0; i < k; i++) {
+            Bus B = Buses.front();
+            WhatEnter(B);
+            cout << "---------Текущее положение ворот, машины и склада с продуктами--------" << endl;
+            this_thread::sleep_for(chrono::milliseconds(10));
+            cout << "BUS № " << i << ", Тонн " << B.countOfTonInABus << ", Продукт № " << B.numberOfTypeProduct << endl << endl;
+            for (int i = 0; i < 3; i++) {
+                Product product = Products[i];
+                cout <<"---PRODUCT на складе, тип №: " << product.whatProduct << " , в количестве " << product.howMuch << " ----" << endl;
+            }
+            for (int i = 0; i < g; i++) {
+                Gate G = Gates[i];
+                this_thread::sleep_for(chrono::milliseconds(10));
+                cout << "GATE № " << i << ", Продукт № " << G.whatProduct  << ", положение " << G.LoadingOrUploading << endl;
+                this_thread::sleep_for(chrono::milliseconds(10));
+            }
+            cout << "-------------------------------------------------------------------" << endl << endl<< endl;
+            Buses.pop();
+        }
+    return Buses;
+};
+
+queue <Bus> BUSES::AddNewBus(queue <Bus> Buses) {
+    Bus bus;
+    bus.countOfTonInABus = 10;
+    bus.numberOfTypeProduct = rand()%3 + 1;
+    Buses.push(bus);
+    return Buses;
+};
+
+Bus BUSES::ReturnFirstBus(queue <Bus> Buses) {
+    Bus firstBus = Buses.front();
+    return firstBus;
+};
+
+void BUSES::WhatEnter (Bus bus) {
+    this_thread::sleep_for(chrono::milliseconds(1000));
+    if (bus.numberOfTypeProduct == 1) {
+        
+        if (Products[0].howMuch < 50) {
+            Gates[1].whatProduct = 1;
+            Gates[1].LoadingOrUploading = 1;
+            Products[0].howMuch += bus.countOfTonInABus;
+            bus.countOfTonInABus = 0;
+        }
+        else if ((Products[0].howMuch < 50 && Gates[0].whatProduct == 1) || Gates[0].howMuch == 0) {
+            Gates[0].whatProduct = 1;
+            Gates[0].LoadingOrUploading = 1;
+            Products[0].howMuch += bus.countOfTonInABus;
+            bus.countOfTonInABus = 0;
+        }
+        else if ((Products[0].howMuch < 50 && Gates[4].whatProduct == 1) || Gates[4].howMuch == 0) {
+            Gates[4].whatProduct = 1;
+            Gates[4].LoadingOrUploading = 1;
+            Products[0].howMuch += bus.countOfTonInABus;
+            bus.countOfTonInABus = 0;
+        }
+    }
+    else if (bus.numberOfTypeProduct == 2) {
+        if (Products[1].howMuch < 50) {
+            Gates[2].whatProduct = 2;
+            Gates[2].LoadingOrUploading = 1;
+            Products[1].howMuch += bus.countOfTonInABus;
+            bus.countOfTonInABus = 0;
+        }
+        else if ((Products[1].howMuch < 50 && Gates[0].whatProduct == 2) || Gates[0].howMuch == 0) {
+            Gates[0].whatProduct = 2;
+            Gates[0].LoadingOrUploading = 1;
+            Products[1].howMuch += bus.countOfTonInABus;
+            bus.countOfTonInABus = 0;
+        }
+        else if ((Products[1].howMuch < 50 && Gates[4].whatProduct == 2) || Gates[4].howMuch == 0) {
+            Gates[4].whatProduct = 2;
+            Gates[4].LoadingOrUploading = 1;
+            Products[1].howMuch += bus.countOfTonInABus;
+            bus.countOfTonInABus = 0;
+        }
+    }
+    else if (bus.numberOfTypeProduct == 3) {
+        if (Products[2].howMuch < 50) {
+            Gates[3].whatProduct = 3;
+            Gates[3].LoadingOrUploading = 1;
+            Products[2].howMuch += bus.countOfTonInABus;
+            bus.countOfTonInABus = 0;
+        }
+        else if ((Products[2].howMuch < 50 && Gates[0].whatProduct == 3) || Gates[0].howMuch == 0) {
+            Gates[0].whatProduct = 3;
+            Gates[0].LoadingOrUploading = 1;
+            Products[2].howMuch += bus.countOfTonInABus;
+            bus.countOfTonInABus = 0;
+        }
+        else if ((Products[2].howMuch < 50 && Gates[4].whatProduct == 3) || Gates[4].howMuch == 0) {
+            Gates[4].whatProduct = 3;
+            Gates[4].LoadingOrUploading = 1;
+            Products[2].howMuch += bus.countOfTonInABus;
+            bus.countOfTonInABus = 0;
+        }
+    }
+    this_thread::sleep_for(chrono::milliseconds(1000));
+    cout << endl << "/////////------Машина выгружена за 2 часа------/////////" << endl << endl;
     
+};
+void BUSES::WhatExit() {
+    this_thread::sleep_for(chrono::milliseconds(2000));
+    int maxOfGates = 0;
+    int max = 0;
+        for (int i=0; i<5; i++){
+            if (Products[i].howMuch >= max) {
+                max = Products[i].howMuch;
+                maxOfGates = i;
+            }
+        }
+    if (Products[maxOfGates].howMuch >= 50) {
+        Products[maxOfGates].howMuch -= 10;
+       // Products[maxOfGates].LoadingOrUploading = 0;
+    }
+};
